e25e68f9df443d902253f7dc9b1ea4d808d81119
Guardian <guardian@mail.bg>
2016-05-06 14:50:36 +0300

    tralala

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -39,7 +39,7 @@
         <div class="container">
             <div class="content">
                 <div class="title">Laravel 5</div>
-		<p>Push this to AWS</p>
+		<p>Push this to AWS!</p>
             </div>
         </div>
     </body>
